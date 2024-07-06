3274390ca376691185458b9d3ef215ad1618dc7b
Willian Fuks <willian.fuks@gmail.com>
2024-07-06 19:39:49 -0300

    hamburger menu installed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useClickAway } from "react-use";
 import Hamburger from "hamburger-react";
 import MaiolnirIcon from "../assets/hammerIcon.svg";
 
 export default function Navbar() {
+
   const [isOpen, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useClickAway(ref, () => {
+    setOpen(false);
+  });
 
   return (
-    <div className="flex h-[60px] items-center justify-center border-b-[1px] border-b-[#F7A712] bg-white">
+    <nav className="border-b-primary fixed flex h-[60px] w-full items-center justify-center border-b-[1px] bg-white">
       <div className="flex translate-x-[-10px] items-center gap-1 ">
         <img className="h-[20px] w-[20px]" src={MaiolnirIcon} />
         <div className="ml-auto mr-auto">
-          <span className="bg-gradient-to-tr from-[#F7A712] via-[#F76C00] to-[#A20021] bg-clip-text text-2xl font-black text-transparent ">
+          <span className="from-accent via-primary to-error bg-gradient-to-tr bg-clip-text text-2xl font-black text-transparent ">
             Maiolnir
           </span>
         </div>
       </div>
-      <div className="absolute right-4">
-        <Hamburger toggled={isOpen} toggle={setOpen} size={20} color="#171717" />
+
+      <div ref={ref} className="absolute right-4">
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          size={20}
+          color="#757780"
+          rounded
+          label="Show menu"
+        />
+        {isOpen && (
+          <div className="bg-info absolute -right-4 top-[54px] h-[100px] w-[100px]"></div>
+        )}
       </div>
-    </div>
+    </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#F76C00',
+        secondary: '#171717',
+        accent: '#F7A712',
+        error: '#A20021',
+        info: '#757780'
+      },
+    },
   },
   plugins: [],
 };
