8fc58ca3186ba0e320c68ae9abc79e5911074375
Willian Fuks <willian.fuks@gmail.com>
2024-07-07 19:24:02 -0300

    mobile menu working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import { useState, useRef } from "react";
 import { useClickAway } from "react-use";
 import Hamburger from "hamburger-react";
 import MaiolnirIcon from "../assets/hammerIcon.svg";
+import { IoHomeOutline } from "react-icons/io5";
+import { SiGoogleearthengine } from "react-icons/si";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Navbar() {
-
   const [isOpen, setOpen] = useState(false);
   const ref = useRef(null);
 
@@ -12,8 +14,21 @@ export default function Navbar() {
     setOpen(false);
   });
 
+  const menu = [
+    {
+      id: "Home",
+      icon: IoHomeOutline,
+      color: "bg-primary",
+    },
+    {
+      id: "How It Works",
+      icon: SiGoogleearthengine,
+      color: "bg-primary",
+    },
+  ];
+
   return (
-    <nav className="border-b-primary fixed flex h-[60px] w-full items-center justify-center border-b-[1px] bg-white">
+    <nav className="border-b-primary fixed z-50 flex h-[60px] w-full items-center justify-center border-b-[1px] bg-white">
       <div className="flex translate-x-[-10px] items-center gap-1 ">
         <img className="h-[20px] w-[20px]" src={MaiolnirIcon} />
         <div className="ml-auto mr-auto">
@@ -28,13 +43,45 @@ export default function Navbar() {
           toggled={isOpen}
           toggle={setOpen}
           size={20}
-          color="#757780"
+          color="#F76C00"
           rounded
           label="Show menu"
         />
-        {isOpen && (
-          <div className="bg-info absolute -right-4 top-[54px] h-[100px] w-[100px]"></div>
-        )}
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 100 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="absolute px-4 -right-4 top-[54px] -z-[50] flex h-[400px] w-[250px] flex-col border border-t-0 border-r-0 bg-white border-primary rounded"
+            >
+              {menu.map((item) => {
+                const Icon = item.icon;
+                return (
+                  <motion.a
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 100 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    onClick={() => setOpen((prev) => !prev)}
+                    className={
+                      "flex w-full items-center justify-start gap-1 border-b border-b-gray-200 p-4 hover:bg-accent hover:bg-opacity-10 active:bg-accent active:text-white rounded"
+                    }
+                    href={`#${item.id.toLowerCase()}`}
+                  >
+                    <Icon
+                      size={23}
+                      color={item.color}
+                      className="text-primary font-black "
+                    />
+                    <span className="text-lg">{item.id}</span>
+                  </motion.a>
+                );
+              })}
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </nav>
   );
