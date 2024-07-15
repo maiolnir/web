d0cf7616dc6bb52f237bb43147902c400e10e4b4
Willian Fuks <willian.fuks@gmail.com>
2024-07-15 19:34:59 -0300

    improve hero

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,11 +1,18 @@
 import { FaArrowRightLong } from "react-icons/fa6";
 
-export default function ContactButton({ size, className }: { size: number, className?: string }) {
+export default function ContactButton({
+  size,
+  className,
+}: {
+  size: number;
+  className?: string;
+}) {
   return (
-    <button className={`${className} bg-accent focus:ring-text group items-center justify-center gap-3 rounded-lg text-lg font-bold text-white transition-all duration-100 hover:bg-yellow-400 focus:outline-none focus:ring-4`}>
+    <button
+      className={`${className} border-accent focus:ring-text group items-center justify-center gap-3 rounded-lg  bg-secondary text-lg font-bold text-white transition-all duration-200 hover:bg-yellow-400 focus:outline-none focus:ring-4`} >
       <span>CONTACT</span>
       <FaArrowRightLong
-        className="transform transition-transform duration-100 group-hover:translate-x-1"
+        className="transform transition-transform duration-200 group-hover:translate-x-1"
         size={size}
       />
     </button>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,30 +5,30 @@ export default function HeroSection() {
   return (
     <>
       <section>
-        <div className="relative h-[415px] w-full bg-gradient-to-br from-[#323232] to-[#313131] px-6 py-10 lg:h-[650px]">
+        <div className="relative h-[415px] w-full bg-gradient-to-br from-[#222] to-[#444] px-6 py-10 lg:h-[650px]">
           <img
-            className="absolute bottom-0 left-1/2 h-[200px] w-[200px] -translate-x-1/2 transform opacity-70 lg:left-auto lg:right-40 lg:h-1/2 lg:translate-x-0 lg:transform"
+            className="absolute bottom-0 left-1/2 h-[200px] w-[200px] -translate-x-1/2 transform opacity-20 lg:left-auto lg:right-40 lg:h-1/2 lg:translate-x-0 lg:transform"
             src={Hammer}
           />
           <div className="container relative mx-auto  h-full font-serif text-[#F8F8F8]">
             <div className="flex flex-col justify-start gap-5 lg:w-1/2 lg:gap-10">
               <h1 className="text-4xl font-black leading-none lg:text-6xl xl:text-[80px]">
                 Customized{" "}
-                <span className="from-accent via-primary bg-gradient-to-r to-red-500 bg-clip-text text-transparent">
+                <span className="from-accent bg-gradient-to-r to-red-400 bg-clip-text text-transparent">
                   Data
                 </span>{" "}
                 Solutions For Your Business
               </h1>
-              <p className="text-[22px] leading-relaxed lg:w-2/3">
+              <p className="text-[20px] lg:text-[22px] leading-relaxed lg:w-2/3">
                 From data engineering to machine learning, we create bespoke
                 solutions that works for you and only{" "}
-                <span className="border-b-1 border-primary rounded border-b p-1">
+                <span className="p-1 from-accent bg-gradient-to-br border-b border-b-1 border-b-primary rounded to-red-500 bg-clip-text text-transparent">
                   you
                 </span>
                 !
               </p>
               <ContactButton
-                className="relative mt-4 flex w-2/3 max-w-[150px] px-1 py-2 lg:px-3 lg:py-4"
+                className="relative mx-auto lg:mx-0 mt-2 lg:-mt-2 flex w-2/3 max-w-[150px] px-1 py-2 lg:px-3 lg:py-4"
                 size={18}
               />
             </div>
