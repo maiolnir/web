eba43ae04f8489461deda8a55f0744f9d06b5227
Willian Fuks <willian.fuks@gmail.com>
2024-07-15 19:35:46 -0300

    implemented summary section

---
+++ src/components/SummarySection.tsx
@@ -0,0 +1,19 @@
+export default function SummarySection() {
+  return (
+    <>
+      <section className="bg-[#F1F1F1] px-1.5 py-2.5 lg:py-10">
+        <div className="container mx-auto pt-6 p-1.5 px-4">
+          <p className="text-[#777] text-base leading-7">
+            Thanks to tools from the open-source community and the power of the
+            cloud, small teams can now implement data analytics solutions that
+            compete with and even outperform those of larger companies.
+            Customization enhances these solutions, driving greater value and
+            results for businesses.{" "}
+            <span className="text-primary">Maiolnir</span> aims at being an
+            efficient provider for customized and performant solutions.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
