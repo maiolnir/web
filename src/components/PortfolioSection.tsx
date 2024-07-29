76135ccf1022ce9916c75c8eb7fa1b5b5bd4f279
Willian Fuks <willian.fuks@gmail.com>
2024-07-29 18:57:58 -0300

    Finish drllama content

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -3,11 +3,13 @@ import { MdDocumentScanner } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 const Card = ({
+  className,
   title,
   src,
   sector,
   path
 }: {
+  className: string,
   title: string;
   src: string;
   sector: string;
@@ -19,11 +21,11 @@ const Card = ({
   };
 
   return (
-    <div className="max-w-1/2 group relative col-span-1 mx-10 h-[400px] overflow-hidden rounded bg-black">
-      <div className="bg-accent flex items-center justify-between p-5 text-2xl font-semibold text-white">
+    <div className={`${className} max-w-1/2 group relative col-span-1 mx-10 h-[400px] overflow-hidden rounded`}>
+      <div className="bg-accent flex items-center justify-between p-3 text-2xl font-semibold text-white">
         <p className="mx-auto">{title}</p>
       </div>
-      <div className="relative">
+      <div className="relative h-full">
         <img className="h-full w-full object-contain" src={src} />
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-70"></div>
       </div>
@@ -31,7 +33,7 @@ const Card = ({
         <p className="text-primary font-semibold">{sector}</p>
         <button
           onClick={() => handleClick(path)}
-          className="focus:ring-text bg-info group flex items-center justify-center gap-3 rounded-lg py-2 text-lg font-bold text-white transition-all duration-200 hover:bg-yellow-400 focus:outline-none focus:ring-4"
+          className="mx-5 focus:ring-text bg-info group flex items-center justify-center gap-3 rounded-lg py-2 text-lg font-bold text-white transition-all duration-200 hover:bg-yellow-400 focus:outline-none focus:ring-4"
         >
           <span className="text-xl font-semibold">Explore</span>
           <FaArrowRightLong
@@ -64,10 +66,18 @@ export default function PortfolioSection() {
         <div className="h-[1000px] bg-gray-700 pt-10">
           <div className="container mx-auto grid grid-cols-1 gap-5 md:grid-cols-3">
             <Card
+              className="bg-black"
               title="Dr.Llama"
               src="https://miro.medium.com/v2/resize:fit:750/format:webp/1*yrWBYgvTjUlvZFD6xpiRjA.jpeg"
               sector="Healthcare Startup"
               path="/portfolio/drllama"
+            />
+            <Card
+              className="bg-white"
+              title="Carrefour: SOT"
+              src="https://seeklogo.com/images/C/Carrefour-logo-DA86663E40-seeklogo.com.png"
+              sector="Retail - online/offline"
+              path="/portfolio/carrefour-sot"
             />
           </div>
         </div>
