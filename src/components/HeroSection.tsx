17afa9a7da72cfd4b274b4786fcc8b33d65e5925
Willian Fuks <willian.fuks@gmail.com>
2024-08-08 20:17:08 -0300

    Add new image for hero banner

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import Hammer from "../assets/manuel-salinas-1pZqh6NVHDc-unsplash.jpg";
+import Hammer from "../assets/maiolnir.jpeg";
 import { ReactNode, useEffect } from "react";
 import ContactButton from "./ContactButton";
 import { motion, useAnimation } from "framer-motion";
@@ -31,7 +31,7 @@ const SlidingDiv = ({
   return (
     <motion.div
       ref={ref}
-      initial={{ x: direction === "left" ? -70 : 70, opacity: 0 }}
+      initial={{ x: direction === "left" ? -40 : 40, opacity: 0 }}
       animate={controls}
     >
       {children}
@@ -43,14 +43,10 @@ export default function HeroSection() {
   return (
     <>
       <section>
-        <div className="relative h-[415px] w-full bg-gradient-to-br from-[#222] to-[#444] px-6 py-10 lg:h-[650px]">
+        <div className="relative h-[415px] w-full bg-gradient-to-br from-[#222] to-[#444] px-6 pt-10 lg:h-[650px]">
           <div className="container relative mx-auto h-full font-serif text-[#F8F8F8] lg:pt-8">
-            <img
-              className="absolute bottom-0 left-1/2 h-[200px] w-[200px] -translate-x-1/2 transform opacity-20 lg:left-auto lg:right-40 lg:h-1/2 lg:translate-x-0 lg:transform"
-              src={Hammer}
-            />
             <SlidingDiv direction="left">
-              <div className="flex flex-col justify-start gap-5 lg:w-1/2 lg:gap-10">
+              <div className="relative z-50 flex flex-col justify-start gap-5 lg:w-1/2 lg:gap-7">
                 <h1 className="text-4xl font-black leading-none lg:text-6xl xl:text-[80px]">
                   Customized{" "}
                   <span className="from-accent bg-gradient-to-r to-red-400 bg-clip-text text-transparent">
@@ -61,17 +57,19 @@ export default function HeroSection() {
                 <p className="text-[20px] leading-relaxed lg:w-2/3 lg:text-[22px]">
                   From data engineering to machine learning, we create bespoke
                   solutions that works for you and only{" "}
-                  <span className="text-accent rounded bg-gray-700 p-1 text-2xl">
-                    you
-                  </span>
-                  !
+                  <span className="text-accent">you</span>!
                 </p>
                 <ContactButton
-                  className="relative mt-2 flex w-2/3 max-w-[150px] px-1 py-2 lg:mx-0 lg:-mt-4 lg:px-3 lg:py-4"
+                  className="relative mt-2 flex w-2/3 max-w-[150px] px-1 py-2 lg:mx-0 lg:-mt-3 lg:px-3 lg:py-4"
                   size={18}
                 />
               </div>
             </SlidingDiv>
+
+            <img
+              className="opacity-20 absolute z-1 bottom-0 left-1/2 h-[200px] w-[200px] -translate-x-1/2 transform rounded lg:opacity-100 lg:left-auto lg:right-10 lg:h-[500px] lg:w-[500px] lg:translate-x-0 lg:transform"
+              src={Hammer}
+            />
           </div>
         </div>
       </section>
