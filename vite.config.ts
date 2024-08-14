3cc77b83e1378ed0b9f235283ae6eca66efb44a2
Willian Fuks <willian.fuks@gmail.com>
2024-08-14 17:45:55 -0300

    Remove .env approach

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), svgr()],
-  base: process.env.VITE_BASE_URL,
-});
+  base: mode === "production" ? "/web/" : "/",
+}));
