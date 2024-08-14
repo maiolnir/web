5291ab56ac5a900fb60d2254b7f0330ce37998a6
Willian Fuks <willian.fuks@gmail.com>
2024-08-14 15:33:32 -0300

    Format main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import MainPage from "./pages/MainPage";
 import MainContent from "./pages/MainContent";
 
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DrLlamaContent from "./pages/DrLlamaContent";
 import CarrefourSOTContent from "./pages/CarrefourSOTContent";
 import WintaylorContent from "./pages/WintaylorContent";
@@ -17,18 +17,23 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage/>} >
-          <Route path="/" element={<MainContent/>} />
+        <Route path="/" element={<MainPage />}>
+          <Route path="/" element={<MainContent />} />
           <Route path="/portfolio/drllama" element={<DrLlamaContent />} />
-          <Route path="/portfolio/carrefour-sot" element={<CarrefourSOTContent />} />
+          <Route
+            path="/portfolio/carrefour-sot"
+            element={<CarrefourSOTContent />}
+          />
           <Route path="/portfolio/wintaylor" element={<WintaylorContent />} />
           <Route path="/portfolio/karza" element={<KarzaContent />} />
           <Route path="/portfolio/cetrix" element={<CetrixContent />} />
           <Route path="/portfolio/gfg-search" element={<GFGSearchContent />} />
-          <Route path="/portfolio/bluemetrics" element={<BlueMetricsContent />} />
+          <Route
+            path="/portfolio/bluemetrics"
+            element={<BlueMetricsContent />}
+          />
         </Route>
       </Routes>
     </Router>
-
   </React.StrictMode>,
 );
