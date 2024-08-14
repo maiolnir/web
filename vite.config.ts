1bc7146795ccb688ff2b2a4e083e05813c8db02a
Willian Fuks <willian.fuks@gmail.com>
2024-08-14 17:33:51 -0300

    base url from .env files

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/web/",
+  base: process.env.VITE_BASE_URL,
 });
