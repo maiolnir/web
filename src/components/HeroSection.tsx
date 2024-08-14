3f0ce82ad1bf974710f994043cb350df58912fd9
Willian Fuks <willian.fuks@gmail.com>
2024-08-14 20:08:30 -0300

    Fix menu bugger bug and contact area bug

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -46,7 +46,7 @@ export default function HeroSection() {
         <div className="relative h-[415px] w-full bg-gradient-to-br from-[#222] to-[#444] px-6 pt-10 lg:h-[650px]">
           <div className="font-poppins container relative mx-auto h-full text-[#F8F8F8] lg:pt-8">
             <SlidingDiv direction="left">
-              <div className="relative z-50 flex flex-col justify-start gap-5 md:mt-4 lg:w-1/2 lg:gap-7">
+              <div className="relative z-30 flex flex-col justify-start gap-5 md:mt-4 lg:w-1/2 lg:gap-7">
                 <h1 className="text-3xl font-bold leading-none lg:text-5xl xl:text-[70px]">
                   Customized{" "}
                   <span className="from-accent bg-gradient-to-r to-red-400 bg-clip-text text-transparent">
@@ -59,9 +59,12 @@ export default function HeroSection() {
                   solutions that works for you and only{" "}
                   <span className="text-accent">you</span>!
                 </p>
-                <a href="https://www.linkedin.com/in/willian-fuks-62622217">
+                <a
+                  className="w-[150px]"
+                  href="https://www.linkedin.com/in/willian-fuks-62622217"
+                >
                   <ContactButton
-                    className="relative mt-2 flex w-2/3 max-w-[150px] px-1 py-2 lg:mx-0 lg:-mt-2 lg:px-3 lg:py-4"
+                    className="relative mt-2 flex px-1 py-2 lg:mx-0 lg:-mt-2 lg:px-3 lg:py-4"
                     size={18}
                   />
                 </a>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,146 +65,142 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="container relative z-50 mx-auto flex h-[72px] w-full items-center bg-white lg:h-[6rem]">
-      <div
-        ref={navRef}
-        className={`${isVisible ? "pointer-events-none opacity-0" : "opacity-100"} bg-primary hover:bg-accent fixed bottom-5 right-5 z-50 flex h-12 w-12 transform items-center justify-center rounded-full transition-opacity duration-300 ease-in-out md:h-16 md:w-16`}
-      >
-        <a href="#">
-          <FaAngleUp color="white" size={30} />
-        </a>
-      </div>
-      <div className="flex h-full w-full items-center">
-        <div className="ml-3 flex flex-col items-center gap-1 lg:ml-0">
-          <img
-            className="h-[20px] w-[20px] lg:h-[30px] lg:w-[30px]"
-            src={MaiolnirIcon}
-          />
-          <div>
-            <HashLink to="/#">
-              <span className="font-medieval from-accent via-primary to-accent font-regular bg-gradient-to-bl bg-clip-text text-xl text-transparent lg:text-2xl">
-                MAIOLNIR
-              </span>
-            </HashLink>
+    <>
+      <header className="container relative z-40 mx-auto flex h-[72px] w-full items-center bg-white lg:h-[6rem]">
+        <div
+          ref={navRef}
+          className={`${isVisible ? "pointer-events-none opacity-0" : "opacity-100"} bg-primary hover:bg-accent fixed bottom-5 right-5 z-50 flex h-12 w-12 transform items-center justify-center rounded-full transition-opacity duration-300 ease-in-out md:h-16 md:w-16`}
+        >
+          <a href="#">
+            <FaAngleUp color="white" size={30} />
+          </a>
+        </div>
+        <div className="flex h-full w-full items-center">
+          <div className="ml-3 flex flex-col items-center gap-1 lg:ml-0">
+            <img
+              className="h-[20px] w-[20px] lg:h-[30px] lg:w-[30px]"
+              src={MaiolnirIcon}
+            />
+            <div>
+              <HashLink to="/#">
+                <span className="font-medieval from-accent via-primary to-accent font-regular bg-gradient-to-bl bg-clip-text text-xl text-transparent lg:text-2xl">
+                  MAIOLNIR
+                </span>
+              </HashLink>
+            </div>
+          </div>
+          <nav className="mx-auto h-full">
+            <ul className="text-text ml-6 hidden h-full items-center lg:flex lg:justify-between lg:gap-10">
+              {menu.map((item) => {
+                const Icon = item.icon;
+                return (
+                  <li
+                    key={item.id}
+                    className="hover:border-accent group flex h-full items-center border-b-4 border-b-transparent pt-3"
+                  >
+                    <HashLink
+                      className="text-text group-hover:text-accent flex items-center gap-1"
+                      to={`/#${item.id.toLowerCase()}`}
+                    >
+                      <Icon size={20} className="pt-0" />
+                      <span className="">{item.id}</span>
+                    </HashLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+
+        {/* Mobile Hamburger Menu and Modal */}
+        <div className="absolute right-4 lg:hidden">
+          <div className="relative z-50">
+            <Hamburger
+              toggled={isOpen}
+              toggle={setOpen}
+              size={24}
+              color="#F76C00"
+              rounded
+              label="Show menu"
+            />
           </div>
         </div>
-        <nav className="mx-auto h-full">
-          <ul className="text-text ml-6 hidden h-full items-center lg:flex lg:justify-between lg:gap-10">
-            {menu.map((item) => {
-              const Icon = item.icon;
-              return (
-                <li
-                  key={item.id}
-                  className="hover:border-accent group flex h-full items-center border-b-4 border-b-transparent pt-3"
-                >
-                  <HashLink
-                    className="text-text group-hover:text-accent flex items-center gap-1"
-                    to={`/#${item.id.toLowerCase()}`}
-                  >
-                    <Icon size={20} className="pt-0" />
-                    <span className="">{item.id}</span>
-                  </HashLink>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-        {/* <div> */}
-        {/*   <ContactButton className="px-6 py-4 lg:flex relative right-14 hidden" size={18} /> */}
-        {/* </div> */}
-      </div>
+      </header>
 
-      {/* Mobile Hamburger Menu and Modal */}
-      <div className="absolute right-4 lg:hidden">
-        <div className="relative z-50">
-          <Hamburger
-            toggled={isOpen}
-            toggle={setOpen}
-            size={24}
-            color="#F76C00"
-            rounded
-            label="Show menu"
-          />
-        </div>
-
-        {/* Modal and Menu */}
-        {isOpen && (
-          <AnimatePresence>
-            <div
-              onClick={() => setOpen(false)}
-              className="fixed bottom-0 left-0 right-0 top-0 bg-gray-500 bg-opacity-30"
-            >
-              <nav className="relative top-[65px] -z-[50] mx-auto w-[100vw] rounded border bg-white px-1">
-                <motion.ul
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 100 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.2 }}
-                  className="flex flex-col"
-                >
-                  {menu.map((item) => {
-                    const Icon = item.icon;
-                    return (
-                      <li
-                        key={item.id}
-                        className="border-b border-b-gray-200 pb-2"
-                      >
-                        <motion.a
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: 100 }}
-                          exit={{ opacity: 0 }}
-                          transition={{ duration: 0.2 }}
-                          onClick={() => setOpen((prev) => !prev)}
-                          className={
-                            "hover:bg-info active:bg-info flex w-full  justify-start gap-2 rounded  pt-3 hover:bg-opacity-10 active:text-white"
-                          }
-                          href={`#${item.id.toLowerCase()}`}
-                        >
-                          <Icon size={30} color="#333" className="pt-0" />
-                          <div className="relative flex w-full items-center justify-between">
-                            <span className="relative  text-2xl font-normal text-[#333]">
-                              {item.id}
-                            </span>
-                            <div className="relative">
-                              <FaChevronRight
-                                size={15}
-                                className="text-[#333]"
-                              />
-                            </div>
-                          </div>
-                        </motion.a>
-                      </li>
-                    );
-                  })}
-                  <li className="border-b border-b-gray-200 pb-2">
-                    <motion.a
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 100 }}
-                      exit={{ opacity: 0 }}
-                      transition={{ duration: 0.2 }}
-                      onClick={() => setOpen((prev) => !prev)}
-                      className={
-                        "hover:bg-info active:bg-info text-primary flex  w-full justify-start gap-2  rounded pt-3 hover:bg-opacity-10 active:text-white"
-                      }
-                      href={`#contact`}
+      {/* Modal and Menu */}
+      {isOpen && (
+        <AnimatePresence>
+          <div
+            onClick={() => setOpen(false)}
+            className="absolute bottom-0 left-0 right-0 top-0 z-50 bg-black bg-opacity-30"
+          >
+            <nav className="absolute top-[65px]  mx-auto w-[100vw] rounded border bg-white px-1">
+              <motion.ul
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 100 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="flex flex-col"
+              >
+                {menu.map((item) => {
+                  const Icon = item.icon;
+                  return (
+                    <li
+                      key={item.id}
+                      className="border-b border-b-gray-200 pb-2"
                     >
-                      <GrContact size={30} className="pt-0" />
-                      <div className="relative flex w-full items-center justify-between">
-                        <span className="relative  text-2xl font-normal">
-                          {"Contact"}
-                        </span>
-                        <div className="relative">
-                          <FaChevronRight size={15} className="text-[#333]" />
+                      <motion.a
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 100 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2 }}
+                        onClick={() => setOpen((prev) => !prev)}
+                        className={
+                          "hover:bg-info active:bg-info flex w-full  justify-start gap-2 rounded  pt-3 hover:bg-opacity-10 active:text-white"
+                        }
+                        href={`#${item.id.toLowerCase()}`}
+                      >
+                        <Icon size={30} color="#333" className="pt-0" />
+                        <div className="relative flex w-full items-center justify-between">
+                          <span className="relative  text-2xl font-normal text-[#333]">
+                            {item.id}
+                          </span>
+                          <div className="relative">
+                            <FaChevronRight size={15} className="text-[#333]" />
+                          </div>
                         </div>
+                      </motion.a>
+                    </li>
+                  );
+                })}
+                <li className="border-b border-b-gray-200 pb-2">
+                  <motion.a
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 100 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    onClick={() => setOpen((prev) => !prev)}
+                    className={
+                      "hover:bg-info active:bg-info text-primary flex  w-full justify-start gap-2  rounded pt-3 hover:bg-opacity-10 active:text-white"
+                    }
+                    href={`#contact`}
+                  >
+                    <GrContact size={30} className="pt-0" />
+                    <div className="relative flex w-full items-center justify-between">
+                      <span className="relative  text-2xl font-normal">
+                        {"Contact"}
+                      </span>
+                      <div className="relative">
+                        <FaChevronRight size={15} className="text-[#333]" />
                       </div>
-                    </motion.a>
-                  </li>
-                </motion.ul>
-              </nav>
-            </div>
-          </AnimatePresence>
-        )}
-      </div>
-    </header>
+                    </div>
+                  </motion.a>
+                </li>
+              </motion.ul>
+            </nav>
+          </div>
+        </AnimatePresence>
+      )}
+    </>
   );
 }
